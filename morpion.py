d86fa0c1a33eae6a00ec5234fcd1238a41d935cb
Cesar Gattano <cesar.gattano@gmail.com>
2025-04-28 16:33:09 +0200

    Ajout de la fonction display_grid dans morpion.py

--- morpion.py
+++ morpion.py
@@ -5,7 +5,17 @@ def create_grid():
     grid["C"] =  ["_", "_", "_"]
     return grid
 
+def display_grid(grid: dict):
+    """Display the grid
+
+    Args:
+        grid (dict): The grid
+    """
+    for line in grid.values():
+        print(line)
+
 def main():
-    print(create_grid())
+    my_grid = create_grid()
+    display_grid(my_grid)
 
 main()
